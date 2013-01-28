427a3cfe195cfbab1584ec4d59640c708abf1eee
Andy Kipp <kipp.andrew@gmail.com>
2013-01-27 21:31:04 -0500

    initial commit

---
+++ RobotEQ.cpp
@@ -0,0 +1,153 @@
+#include "Arduino.h"
+#include "RobotEQ.h"
+
+#define ETIMEOUT -1 
+#define EIO -2
+
+RobotEQ::RobotEQ(Stream *serial) {
+	m_Timeout = ROBOTEQ_DEFAULT_TIMEOUT;	
+	m_Serial = serial; 
+}
+
+RobotEQ::~RobotEQ(void) {
+}
+
+void RobotEQ::setTimeout(uint32_t timeout) {
+	m_Timeout = timeout;
+}
+
+int RobotEQ::isConnected() {
+	return this->isConnected(this->m_Timeout);
+}
+
+int RobotEQ::isConnected(uint32_t timeout) {
+	if (this->m_Serial == NULL) 
+		return EIO;
+	
+	uint8_t inByte = 0;
+	uint32_t startTime = millis();
+
+	// send QRY 
+	this->send(ROBOTEQ_QUERY_CHAR);
+
+	while (millis() - startTime < timeout) {
+		// wait for ACK
+		if (m_Serial->available() > 0) {
+			inByte = m_Serial->read();
+			if (inByte == ROBOTEQ_ACK_CHAR) {
+				return 0;
+			}
+		}
+	   	delay(100);	
+	}
+	return ETIMEOUT;
+}
+		
+int RobotEQ::commandMotorPower(uint8_t ch, int16_t p) {
+	char command[32];
+	sprintf(command, "!G %02d %d\r", ch, p);
+	return this->sendCommand(command);
+}
+
+int RobotEQ::queryFaultFlag(RobotEQFaultFlag flag) {
+	uint8_t fault;
+	if (this->sendQuery("?FF\r", &fault, 1) <= 0) {
+		return -1;
+	}
+	return (fault & (1 << (int)flag) > 0);
+}
+
+int RobotEQ::queryStatusFlag(RobotEQStatusFlag flag) {
+	uint8_t status;
+	if (this->sendQuery("?FS\r", &status, 1) <= 0) {
+		return -1;
+	}
+	return (status & (1 << (int)flag) > 0);
+}
+
+int RobotEQ::queryFirmware(char *buf, size_t size) {
+	return this->sendQuery("?FID\r", (uint8_t*)buf, size);
+}
+
+int RobotEQ::sendCommand(const char *str) {
+	return this->sendCommand((uint8_t*)str, strlen(str));
+}
+		
+int RobotEQ::sendCommand(const uint8_t *q, size_t qSize) {
+	return this->sendCommand(q, qSize, this->m_Timeout);
+}
+
+int RobotEQ::sendCommand(const uint8_t *q, size_t qSize, uint32_t timeout) {
+	if (this->m_Serial == NULL) 
+		return EIO;
+
+	uint8_t buf[32];
+
+	// Write Command to Serial
+	this->send(q, qSize);
+
+	// Read Serial until timeout or newline
+	if (this->readSerialUntilNewline(buf, 32) <= 0) {
+		return -1;
+	}	
+
+	// Check Command Status
+	if (strncmp((char*)buf, "+", 1) == 0) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+int RobotEQ::sendQuery(const char *str, uint8_t *r, size_t rSize) {
+	return this->sendQuery((const uint8_t*)str, strlen(str), r, rSize);
+}
+
+int RobotEQ::sendQuery(const uint8_t *q, size_t qSize, uint8_t *r, size_t rSize) {
+	return this->sendQuery(q, qSize, r, rSize, this->m_Timeout);
+}
+
+int RobotEQ::sendQuery(const uint8_t *q, size_t qSize, uint8_t *r, size_t rSize, uint32_t timeout) {
+	if (this->m_Serial == NULL) 
+		return EIO;
+
+	// Write Query to Serial
+	this->send(q, qSize);
+
+	// Read Serial until timeout or newline
+	return this->readSerialUntilNewline(r, rSize);
+}
+
+int RobotEQ::send(const uint8_t b) {
+	Log.Debug("sending to controller: %X"CR, (char)b);	
+	this->m_Serial->write(b);
+	this->m_Serial->flush();
+}
+
+int RobotEQ::send(const uint8_t * buf, size_t size) {
+	Log.Debug("sending to controller: %s"CR, (char*)buf);	
+	this->m_Serial->write(buf, size);
+	this->m_Serial->flush();
+}
+
+int RobotEQ::readSerialUntilNewline(uint8_t * buf, size_t size) {
+	return this->readSerialUntilNewline(buf, size, this->m_Timeout);
+}
+
+int RobotEQ::readSerialUntilNewline(uint8_t * buf, size_t size, uint32_t timeout) {
+	uint8_t inByte;
+	size_t index = 0;
+	uint32_t startTime = millis();	
+	while (millis() - startTime < timeout) {
+		if (m_Serial->available() > 0) {
+			inByte = m_Serial->read();
+			Log.Debug("read %X from controller"CR, inByte);
+			buf[index++] = inByte;
+			if (inByte == 0x0D) 
+				return index;
+		}
+	}
+	// timeout
+	Log.Error("timeout reading controller"CR);
+	return ETIMEOUT;
+}
---
+++ RobotEQ.h
@@ -0,0 +1,106 @@
+// Copyright (C) 2013 Andy Kipp <kipp.andrew@gmail.com> 
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#ifndef ROBOTEQ_H
+#define ROBOTEQ_H
+
+#define ROBOTEQ_QUERY_CHAR 0x05
+#define ROBOTEQ_ACK_CHAR 0x06
+#define ROBOTEQ_DEFAULT_TIMEOUT 1000 
+
+#include "Logging.h"
+#include <HardwareSerial.h>
+
+enum RobotEQFaultFlag {
+	Overheat = 1,
+	OverVoltage = 2,
+	UnderVoltage = 3,
+	Short = 4,
+	EmergencyStop = 5,
+	ScriptExecFault = 6,
+	MOSFETFailure = 7,
+	StartupConfigurationFault = 8
+};
+
+enum RobotEQStatusFlag {
+	SerialMode = 1,
+	PulseMode = 2,
+	AnalogMode = 3,
+	PowerStageOff = 4,
+	StallDetected = 5,
+	AtLimit = 6,
+	ScriptRunning = 8
+};
+
+class RobotEQ {
+	// Constructors
+	public:
+		RobotEQ(Stream *serial);
+
+	// Destructors
+	public:
+		~RobotEQ(void);
+
+	// Public Methods
+	public:
+		void setTimeout(uint32_t timeout);
+
+		int isConnected(void);
+		int isConnected(uint32_t timeout);
+
+		int commandMotorPower(uint8_t ch, int16_t p);
+
+		int queryMotorAmps(uint8_t ch);
+		int queryMotorPower(uint8_t ch);
+
+		int queryBatteryAmps(void);
+		int queryBatteryVoltage(void);
+
+		int queryFaultFlag(RobotEQFaultFlag flag);
+		int queryStatusFlag(RobotEQStatusFlag flag);
+
+		int queryFirmware(char* buf, size_t size);
+
+	// Private Methods
+	private:
+
+		int sendQuery(const char *str, uint8_t *r, size_t rSize); 
+		int sendQuery(const uint8_t *q, size_t qSize, uint8_t *r, size_t rSize);
+		int sendQuery(const uint8_t *q, size_t qSize, uint8_t *r, size_t rSize, uint32_t timeout);
+
+		int sendCommand(const char *str);
+		int sendCommand(const uint8_t *q, size_t qSize);
+		int sendCommand(const uint8_t *q, size_t qSize, uint32_t timeout);
+		
+		int send(const uint8_t b);
+		int send(const uint8_t * buf, size_t size);
+
+		int readSerialUntilNewline(uint8_t * buf, size_t size);
+		int readSerialUntilNewline(uint8_t * buf, size_t size, uint32_t timeout);
+
+	// Private Data
+	private:
+		uint32_t 	m_Timeout;
+		Stream		*m_Serial; 
+};
+
+#endif
+
